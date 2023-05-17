41d3084535b9d32b61d3b1e72063058dae5240bf
unknown <brandonquintero2005@gmail.com>
2023-05-17 17:50:40 -0500

    validaciones insumos

--- src/components/Admin/Insumos/CrearInsumo.js
+++ src/components/Admin/Insumos/CrearInsumo.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import Swal from 'sweetalert2';
 import { ValidInsumos } from "./ValidInsumos";
 
 
@@ -20,8 +21,27 @@ function CrearInsumo() {
         setErrors(ValidInsumos(values));
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (errors.nombreInsumo === "" && errors.descInsumo === "" && errors.precioInsumo === "") {
+            Swal.fire({
+                title: 'Creado Correctamente',
+                text: "Tu insumo ha sido creado correctamente",
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 1500
+              })
+            setTimeout(function(){ window.location = "insumos"; }, 1000);
+        }
+    }
+
+    function refreshPage() {
+        window.location.reload(false);
+      }
+
     return(
-                    <div id="crear">
+        <div>
+            <form onSubmit={handleSubmit}>
                         <br/>
                         <h2 class="text-black" id="title">Crear Insumo</h2>
                             <div class="form-group">
@@ -31,19 +51,23 @@ function CrearInsumo() {
                             </div>
                             <div class="form-group">
                                 <label for="descInsumo">Descripción</label>
-                                <input type="text" class="form-control" id="descInsumo" name="descInsumo"/>
+                                <input type="text" class="form-control" id="descInsumo" name="descInsumo" onChange={handleInput} onBlur={handleBlur}/>
+                                {errors.descInsumo && <span className="text-danger"> {errors.descInsumo}</span>}
                             </div>
                             <div class="form-group">
                                 <label for="precioInsumo">Precio</label>
-                                <input type="text" class="form-control" id="precioInsumo" name="precioInsumo"/>
+                                <input type="text" class="form-control" id="precioInsumo" name="precioInsumo" onChange={handleInput} onBlur={handleBlur}/>
+                                {errors.precioInsumo && <span className="text-danger"> {errors.precioInsumo}</span>}
                             </div>
                             <div class="form-check" style={{marginBottom: '7px'}}>
                                 <input type="checkbox" class="form-check-input" id="estadoInsumo" name="estadoInsumo"/>
                                 <label class="form-check-label" for="estadoInsumo">Disponible</label>
                             </div>
                             <button type="submit" class="btn btn-primary" id="crearInsumo">Crear</button> &nbsp;
-                            <button type="submit" class="btn btn-dark" id="cancelarInsumo">Cancelar</button>
-                    </div>  
+                            <input type="button" class="btn btn-dark" id="cancelarInsumo" onClick={refreshPage} value="Cancelar"/>
+                    </form>
+        </div>
+                    
     );
 }
 export { CrearInsumo }
