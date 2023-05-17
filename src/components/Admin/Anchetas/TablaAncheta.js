c3526aeb49246d9cb2ab0cb6e0c707312043ea11
Salgado-2022 <salgadojuandavid419@gmail.com>
2023-05-16 19:02:32 -0500

    Cambios de brandon en tabla anchetas

--- src/components/Admin/Anchetas/TablaAncheta.js
+++ src/components/Admin/Anchetas/TablaAncheta.js
@@ -7,8 +7,8 @@ function TablaAncheta(){
                             <div className="input-group mb-3 col-6">
                                 <input type="text" className="form-control" placeholder="Buscar Ancheta"/>
                                 <div className="input-group-append">
-                                    <button className="btn btn-outline" type="button"><a href="#"
-                                            className="icon-search"></a></button>
+                                    <button className="btn btn-outline" type="button"><a href="#!"
+                                            className="icon-search"> </a></button>
                                 </div>
                             </div>
                         </div>
@@ -29,55 +29,55 @@ function TablaAncheta(){
                                 <tr>
                                     <th scope="row">1</th>
                                     <td>Ancheta Navideña</td>
-                                    <td><a href="#" className="icon-eye" data-toggle="modal"
-                                            data-target="#modalAnchetaDetalle"></a></td>
+                                    <td><a href="#!" className="icon-eye" data-toggle="modal"
+                                            data-target="#modalAnchetaDetalle"> </a></td>
                                     <td>90.000</td>
                                     <td>Disponible</td>
-                                    <td><a href="#" className="icon-edit" onclick="modificarAncheta()"></a></td>
-                                    <td><a href="#" className="icon-trash" id="eliminar" onclick="eliminarAncheta()"></a>
+                                    <td><a href="#!" className="icon-edit" onclick="modificarAncheta()"> </a></td>
+                                    <td><a href="#!" className="icon-trash" id="eliminar" onclick="eliminarAncheta()"> </a>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th scope="row">2</th>
                                     <td>Ancheta Halloween</td>
-                                    <td><a href="#" className="icon-eye" data-toggle="modal"
-                                            data-target="#modalAnchetaDetalle"></a></td>
+                                    <td><a href="#!" className="icon-eye" data-toggle="modal"
+                                            data-target="#modalAnchetaDetalle"> </a></td>
                                     <td>135.000</td>
                                     <td>Disponible</td>
-                                    <td><a href="#" className="icon-edit" onclick="modificarAncheta()"></a></td>
-                                    <td><a href="#" className="icon-trash" id="eliminar" onclick="eliminarAncheta()"></a>
+                                    <td><a href="#!" className="icon-edit" onclick="modificarAncheta()"> </a></td>
+                                    <td><a href="#!" className="icon-trash" id="eliminar" onclick="eliminarAncheta()"> </a>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th scope="row">3</th>
                                     <td>Ancheta Padres</td>
-                                    <td><a href="#" className="icon-eye" data-toggle="modal"
-                                            data-target="#modalAnchetaDetalle"></a></td>
+                                    <td><a href="#!" className="icon-eye" data-toggle="modal"
+                                            data-target="#modalAnchetaDetalle"> </a></td>
                                     <td>42.000</td>
                                     <td>Agotado</td>
-                                    <td><a href="#" className="icon-edit" onclick="modificarAncheta()"></a></td>
-                                    <td><a href="#" className="icon-trash" id="eliminar" onclick="eliminarAncheta()"></a>
+                                    <td><a href="#!" className="icon-edit" onclick="modificarAncheta()"> </a></td>
+                                    <td><a href="#!" className="icon-trash" id="eliminar" onclick="eliminarAncheta()"> </a>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th scope="row">4</th>
                                     <td>Ancheta Madres</td>
-                                    <td><a href="#" className="icon-eye" id="eliminar" onclick="eliminarAncheta()"></a></td>
+                                    <td><a href="#!" className="icon-eye" id="eliminar" onclick="eliminarAncheta()"> </a></td>
                                     <td>42.000</td>
                                     <td>Agotado</td>
-                                    <td><a href="#" className="icon-edit" onclick="modificarAncheta()"></a></td>
-                                    <td><a href="#" className="icon-trash" id="eliminar" onclick="eliminarAncheta()"></a>
+                                    <td><a href="#!" className="icon-edit" onclick="modificarAncheta()"> </a></td>
+                                    <td><a href="#!" className="icon-trash" id="eliminar" onclick="eliminarAncheta()"> </a>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th scope="row">5</th>
                                     <td>Ancheta Cumpleañera</td>
-                                    <td><a href="#" className="icon-eye" data-toggle="modal"
-                                            data-target="#modalAnchetaDetalle"></a></td>
+                                    <td><a href="#!" className="icon-eye" data-toggle="modal"
+                                            data-target="#modalAnchetaDetalle"> </a></td>
                                     <td>67.000</td>
                                     <td>Agotado</td>
-                                    <td><a href="#" className="icon-edit" onclick="modificarAncheta()"></a></td>
-                                    <td><a href="#" className="icon-trash" id="eliminar" onclick="eliminarAncheta()"></a>
+                                    <td><a href="#!" className="icon-edit" onclick="modificarAncheta()"> </a></td>
+                                    <td><a href="#!" className="icon-trash" id="eliminar" onclick="eliminarAncheta()"> </a>
                                     </td>
                                 </tr>
                             </tbody>
