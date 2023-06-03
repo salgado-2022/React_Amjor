39b95bf932932f37af08b7b5b7546b554e0f5e2e
Salgado-2022 <salgadojuandavid419@gmail.com>
2023-06-02 22:38:49 -0500

    Cambio de nomber en unas variables

--- src/views/Admin/Pedidos.js
+++ src/views/Admin/Pedidos.js
@@ -3,13 +3,9 @@ import { Table } from "../../components/Admin/Pedidos/Table";
 
 function Pedidos() {
     return (
-        <div className="site-wrap">
-            <div className="site-section">
                 <div className="container">
                     <Table />
                 </div>
-            </div>
-        </div>
     );
 }
 export { Pedidos }
