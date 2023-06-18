b9ab2b314188d3e40da289ec5a64a247961254ce
juandaxx2343 <juandathebass@gmail.com>
2023-06-17 19:56:55 -0500

    cambios

--- src/components/Admin/Configuracion/ListaConfiguracion.js
+++ src/components/Admin/Configuracion/ListaConfiguracion.js
@@ -14,8 +14,8 @@ function ListaConfiguracion() {
 
   return (
     <>
-      <div className="site-section"></div>
-      <div className="container"></div>
+      <div className="site-section"> 
+      <div className="container"> 
       <div className="col-12">
         <div className="row justify-content-end">
           <div className="input-group mb-3 col-3">
@@ -24,63 +24,65 @@ function ListaConfiguracion() {
           </div>
         </div>
       </div>
-      <div className="row">
-        <div></div>
-      </div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Rol</th>
-            <th scope="col">Permisos</th>
-            <th scope="col">Estado</th>
-            <th scope="col">Estado</th>
-            <th scope="col">Editar</th>
-            <th scope="col">Eliminar</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Administrador</td>
-            <td>Todo el aplicativo</td>
-            <td>Activo y listo pa dal</td>
-            <td>
-              <label className="switch">
-                <input type="checkbox" />
-                <span className="slider"></span>
-              </label>
-            </td>
-            <td>
-              <a href="#!" className="icon-edit" onClick={handleEditarClick}> </a>
-            </td>
-            <td>
-              <a href="#!" className="icon-trash" onClick="eliminarUsuario()"> </a>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Cliente</td>
-            <td>Anchetas, insumos, ventas</td>
-            <td>Activo</td>
-            <td>
-              <label className="switch">
-                <input type="checkbox" />
-                <span className="slider"></span>
-              </label>
-            </td>
-            <td>
-              <a href="#!" className="icon-edit" onClick={handleEditarClick}> </a>
-            </td>
-            <td>
-              <a href="#!" className="icon-trash" onClick="eliminarUsuario()"> </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+          <div className="row">
+            <div></div>
+          </div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Rol</th>
+                <th scope="col">Permisos</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Eliminar</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Administrador</td>
+                <td>Todo el aplicativo</td>
+                <td>Activo y listo pa dal</td>
+                <td>
+                  <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider"></span>
+                  </label>
+                </td>
+                <td>
+                  <a href="#!" className="icon-edit" onClick={handleEditarClick}> </a>
+                </td>
+                <td>
+                  <a href="#!" className="icon-trash" onClick="eliminarUsuario()"> </a>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Cliente</td>
+                <td>Anchetas, insumos, ventas</td>
+                <td>Activo</td>
+                <td>
+                  <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider"></span>
+                  </label>
+                </td>
+                <td>
+                  <a href="#!" className="icon-edit" onClick={handleEditarClick}> </a>
+                </td>
+                <td>
+                  <a href="#!" className="icon-trash" onClick="eliminarUsuario()"> </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
 
-      {/* Ventana modal */}
-      {showModal && <EditarConfiguracion onClose={handleCloseModal} />}
+          {/* Ventana modal */}
+          {showModal && <EditarConfiguracion onClose={handleCloseModal} />}
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Admin/Usuarios/modal/editarUsuario.js
+++ src/components/Admin/Usuarios/modal/editarUsuario.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
-import Modal  from 'react-bootstrap/Modal';
+import Modal from 'react-bootstrap/Modal';
 import { Button } from "react-bootstrap";
 import Swal from 'sweetalert2';
 import axios from "axios";
 
 function EditarUsuario(props) {
-  const { selectedUsuarioID, onHide, show  } =props;
+  const { selectedUsuarioID, onHide, show } = props;
   const id = selectedUsuarioID;
 
-  const [isChecked, setIsChecked] = useState(false); // false = 0
-
+  const [isChecked, setIsChecked] = useState(false);
 
   const [values, setValues] = useState({
     correo: '',
@@ -20,91 +19,88 @@ function EditarUsuario(props) {
     const { name, value, type, checked } = event.target;
 
     if (type === 'checkbox') {
-        setIsChecked(checked);
-        setValues(prev => ({ ...prev, [name]: checked ? 1 : 0 }));
+      setIsChecked(checked);
+      setValues(prev => ({ ...prev, [name]: checked ? 1 : 0 }));
     } else {
-        setValues(prev => ({ ...prev, [name]: value }));
+      setValues(prev => ({ ...prev, [name]: value }));
     }
   };
 
   useEffect(() => {
     if (show) {
-        axios.get('http://localhost:4000/api/admin/usuario/usullamada/' + id)
-            .then(res => {
-                console.log(res);
-                setValues(prevValues => ({
-                    ...prevValues,
-                    correo: res.data[0].correo,
-                    contrasena: res.data[0].contrasena
-                   
-
-                }));
-                setIsChecked(res.data[0].ID_Usuario === 1);
-            })
-            .catch(err => console.log(err));
+      axios.get(`http://localhost:4000/api/admin/usuario/usullamada/${id}`)
+        .then(res => {
+          console.log(res);
+          setValues(prevValues => ({
+            ...prevValues,
+            correo: res.data[0].correo,
+            contrasena: res.data[0].contrasena
+          }));
+          setIsChecked(res.data[0].ID_Usuario === 1);
+        })
+        .catch(err => console.log(err));
     }
   }, [id, show]);
 
   const handleUpdate = (event) => {
     event.preventDefault();
-    axios.put('http://localhost:4000/api/admin/usuario/usuariarioedit/' + id, values)
-        .then(res => {
-            console.log(res);
-            Swal.fire({
-                title: 'Modificado Correctamente',
-                text: "Tu Usuario se ha sido modificado correctamente",
-                icon: 'success',
-                showConfirmButton: false,
-                timer: 1500
-            });
-            setTimeout(function () { window.location = "usuarios"; }, 670);
-        })
-        .catch(err => console.log(err));
+    axios.put(`http://localhost:4000/api/admin/usuario/usuariarioedit/${id}`, values)
+      .then(res => {
+        console.log(res);
+        Swal.fire({
+          title: 'Modificado Correctamente',
+          text: "Tu Usuario se ha sido modificado correctamente",
+          icon: 'success',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        setTimeout(function () { window.location = "usuario"; }, 670);
+      })
+      .catch(err => console.log(err));
   };
 
   return (
     <Modal
-            onHide={onHide}
-            show={show}
-            size="lg" 
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            style={{ zIndex: '2000', boxShadow: '0 0 10px MediumSlateBlue' }}
-        >
-          <Modal.Header>
-                <Modal.Title id="contained-modal-title-vcenter" className="text-black">
-                    Editar datos de Usuario
-                </Modal.Title>
-                <Button variant="secondary" onClick={props.onHide} className="close">
-                    <span aria-hidden="true">&times;</span>
-                </Button>
-            </Modal.Header>
+      onHide={onHide}
+      show={show}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      style={{ zIndex: '2000', boxShadow: '0 0 10px MediumSlateBlue' }}
+    >
+      <Modal.Header>
+        <Modal.Title id="contained-modal-title-vcenter" className="text-black">
+          Editar datos de Usuario
+        </Modal.Title>
+        <Button variant="secondary" onClick={props.onHide} className="close">
+          <span aria-hidden="true">&times;</span>
+        </Button>
+      </Modal.Header>
       <Modal.Body>
-      <div>
-                    <form onSubmit={handleUpdate} id="editarUsuario">
-                        <div className="form-group">
-                            <label htmlFor="correo">Correo</label>
-                            <input type="text" className="form-control" id="correo" name="correo" value={values.correo} onChange={handleInput} />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="contrasena">Contraseña</label>
-                            <input type="password" className="form-control" id="contrasena" name="contrasena" value={values.contrasena} onChange={handleInput} />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="conficontrasena">Confirmar Contraseña</label>
-                            <input type="password" className="form-control" id="contrasena" name="contrasena" value={values.contrasena} onChange={handleInput} />
-                        </div>
-                        <div className="form-check" style={{ marginBottom: '7px' }}>
-                            <input type="checkbox" className="form-check-input" id="ID_Estado" name="ID_Estado" checked={isChecked} onChange={handleInput} />
-                            <label className="form-check-label" htmlFor="estadoUsuarios">Disponible</label>
-                        </div>
-                        <button type="submit" className="btn btn-primary" id="modUsuario" onClick={props.onHide}>Modificar</button> &nbsp;
-                        <button type="reset" className="btn btn-dark" id="cancelarUsuario" onClick={props.onHide}>Cancelar</button>
-                    </form>
-                </div>
+        <div>
+          <form onSubmit={handleUpdate} id="editarUsuario">
+            <div className="form-group">
+              <label htmlFor="correo">Correo</label>
+              <input type="text" className="form-control" id="correo" name="correo" value={values.correo} onChange={handleInput} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="contrasena">Contraseña</label>
+              <input type="password" className="form-control" id="contrasena" name="contrasena" value={values.contrasena} onChange={handleInput} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="conficontrasena">Confirmar Contraseña</label>
+              <input type="password" className="form-control" id="conficontrasena" name="conficontrasena" value={values.conficontrasena} onChange={handleInput} />
+            </div>
+            <div className="form-check" style={{ marginBottom: '7px' }}>
+              <input type="checkbox" className="form-check-input" id="ID_Estado" name="ID_Estado" checked={isChecked} onChange={handleInput} />
+              <label className="form-check-label" htmlFor="estadoUsuarios">Disponible</label>
+            </div>
+            <button type="submit" className="btn btn-primary" id="modUsuario" onClick={props.onHide}>Guardar Cambios</button> &nbsp;
+            <button type="reset" className="btn btn-dark" id="cancelarUsuario" onClick={props.onHide}>Cancelar</button>
+          </form>
+        </div>
       </Modal.Body>
     </Modal>
   );
 }
-
 export { EditarUsuario };
