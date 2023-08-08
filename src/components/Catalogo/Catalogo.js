6a34c092909d831d6f50c4b114f1c9501962fca7
unknown <brandonquintero2005@gmail.com>
2023-08-08 09:52:13 -0500

    modal catalogue

--- src/components/Catalogo/Catalogo.js
+++ src/components/Catalogo/Catalogo.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
-import { useCart } from '../../hooks/useCart'
 import axios from "axios";
+import { useCart } from '../../hooks/useCart';
+import { AnchetaDetalle } from "./AnchetaDetalle";
+
 
 function ProductosCatalogo({ products }) {
     const { addToCart, removeFromCart, cart } = useCart()
@@ -9,10 +10,11 @@ function ProductosCatalogo({ products }) {
     const checkProductInCart = product => {
         return cart.some(item => item.ID_Ancheta === product.ID_Ancheta)
     }
-    
+
 
     const [data, setData] = useState([]);
     const [selectedAnchetaID, setSelectedAnchetaID] = useState(null);
+    const [modalShow, setModalShow] = React.useState(false);
 
     const formatPrice = (price) => {
         return price.toLocaleString('es-CO', {
@@ -24,6 +26,7 @@ function ProductosCatalogo({ products }) {
 
     const handleAnchetaClick = (anchetaID) => {
         setSelectedAnchetaID(anchetaID);
+        setModalShow(true);
     };
 
     const fetchData = () => {
@@ -38,11 +41,8 @@ function ProductosCatalogo({ products }) {
         fetchData();
     }, []);
 
-
-    console.log(data)
-
-
     return (
+        <>
         <div className="row mb-5">
             {products.map((product) => {
                 const isProductInCart = checkProductInCart(product)
@@ -52,38 +52,22 @@ function ProductosCatalogo({ products }) {
                         <div className="block-4 card catalogue" onClick={() => { handleAnchetaClick(product.ID_Ancheta) }} style={{ borderRadius: "5%", boxShadow: "0 2px 15px rgba(0, 0, 0, 0.1)", border: "none", cursor: "pointer" }}>
                             <img src={`http://localhost:4000/anchetas/` + product.image} alt="" className="card-img-top img-fluid size-catalog block-4-image" />
                             <div className="card-body">
-                                <h3 className="card-title" style={{ color: "Black", fontSize: "16px", marginTop: "5px" }}>{product.NombreAncheta}</h3>
+                                <h3 className="card-title" style={{  color: "Black", fontSize: "16px", marginTop: "5px" }}>{product.NombreAncheta}</h3>
                                 <p className="card-text text-right font-weight-normal" style={{ color: "MediumSlateBlue", fontSize: "18px" }}>{formatPrice(product.PrecioUnitario)}</p>
-                                <button onClick={() => isProductInCart
-                                     ? removeFromCart(product) 
-                                     : addToCart(product)} style={{ backgroundColor: isProductInCart ? 'red' : 'blue'}}>
-                                    
-                                    Añadir al carrito</button>
+                                <button className="btn"onClick={(e) => {e.stopPropagation(); isProductInCart ? removeFromCart(product) : addToCart(product);}} style={{ backgroundColor: isProductInCart ? 'red' : 'MediumSlateBlue'}}>Añadir al carrito</button>
                             </div>
                         </div>
                     </div>
                 )
             })}
         </div>
+        <AnchetaDetalle
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        selectedAnchetaID={selectedAnchetaID}
+        />
+        </>
     );
-
-    
-
-//     return (
-//         <div className="row mb-5">
-//             {data.map((anchetas) => (
-//                 <div className="col-sm-6 col-lg-4 mb-4" data-aos="fade-up" key={anchetas.ID_Ancheta} onClick={fetchData}>
-//                     <div className="block-4 card catalogue" onClick={() => { handleAnchetaClick(anchetas.ID_Ancheta) }} style={{ borderRadius: "5%", boxShadow: "0 2px 15px rgba(0, 0, 0, 0.1)", border: "none", cursor: "pointer" }}>
-//                         <img src={`http://localhost:4000/anchetas/` + anchetas.image} alt="" className="card-img-top img-fluid size-catalog block-4-image" />
-//                         <div className="card-body">
-//                             <h3 className="card-title" style={{ color: "Black", fontSize: "16px", marginTop: "5px" }}>{anchetas.NombreAncheta}</h3>
-//                             <p className="card-text text-right font-weight-normal" style={{ color: "MediumSlateBlue", fontSize: "18px" }}>{formatPrice(anchetas.PrecioUnitario)}</p>
-//                         </div>
-//                     </div>
-//                 </div>
-//             ))}
-//         </div>
-//     );
 }
 
 export { ProductosCatalogo };
